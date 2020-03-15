21f0261f60432244e9637a54f8c04d271ef3c814
jonasgadeikis <jonasgadeikis.github@gmail.com>
2020-03-15 12:22:30 +0200

    Feature JWT Authentication

--- core/config/bundles.php
+++ core/config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
